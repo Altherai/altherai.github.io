f4a147bbdb0233be65245c3f62b1325e1ef6a0e0
Altherai <alex.altherai@gmail.com>
2026-09-17 08:23:47 +0300

    feat: add contact and social data

---
+++ src/data/socials.ts
@@ -0,0 +1,6 @@
+export const contactEmail = 'alex.altherai@gmail.com'
+
+export const socialLinks = [
+  { label: 'YouTube', href: 'https://www.youtube.com/@bugsbunny6251' },
+  { label: 'Website', href: 'https://altherai.github.io' },
+]
